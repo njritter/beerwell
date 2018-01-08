6f3b8652962dee8817ee05ed65a939b6f5935143
Neil Ritter <ritter.neil@gmail.com>
2018-01-07 21:15:28 -0600

    Database and registration/login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
 
-from app import routes # routes needs to import 'app' variable defined above
+from app import routes, models # routes needs to import 'app' variable
--- beerwell.py
+++ beerwell.py
@@ -1,1 +1,6 @@
-from app import app # import application instance defined in __init__.py
+from app import app, db # import application instance defined in __init__.py
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
